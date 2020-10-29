e848456dc37ce91fb301be33e64afd7b03aff230
Jesus Romero <jesusenrique150620@gmail.com>
2020-10-29 16:54:29 -0400

    Adding login

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs/dist/bcrypt");
+
 var responses = {
     messageErr:"",
     messageOK:"",
@@ -15,7 +17,7 @@ var express               = require("express"),
     Bcrypt                = require("bcryptjs"),
     jwt                   = require("jsonwebtoken")  
     Sql                   = require("mysql"),
-    User                  = require("./models/user");
+    cookieParser          = require("cookie-parser")
 
 //Config
 app.set("port",process.env.PORT||3002);
@@ -25,6 +27,7 @@ app.use(express.static("public"));
 app.use(FP());
 app.use(MethodOverride("_method"));
 app.use(flash());
+app.use(cookieParser());
 
 //conexion a mysql//
 var DB = Sql.createConnection({
@@ -47,7 +50,51 @@ app.get("/home", function(req, res){
 })
 
 app.get("/login", function(req, res){
-    res.render("login");
+    res.render("login", {responses});
+    responses.messageErr = "";
+    responses.messageOk = "";
+})
+
+app.post("/login", function(req, res){
+    loginData = req.body.data;
+    console.log(loginData);
+    DB.query("SELECT id, correo, clave from usuarios WHERE correo = ?", [loginData.email], async (error, results) => {
+        console.log(results);
+        if(error) console.log(error);
+        if(results.length > 0) {
+            await bcrypt.compare(loginData.pass, results[0].clave, function(err, result) {
+                if(err) console.log(err);
+                if(result) {
+                    const id = results[0].id;
+
+                    const token = jwt.sign({ id }, process.env.JWT_SECRET||"mysupersecretpassword", {
+                        expiresIn: process.env.JWT_EXPIRES_IN||"2h"
+                    });
+
+                    console.log(`The token is ${token}`);
+
+                    const cookieOptions = {
+                        expires: new Date(
+                            Date.now() + process.env.JWT_COOKIE_EXPIRES||2 * 60 * 60 * 1000
+                        ),
+                        httpOnly: true
+                    }
+                    
+                    res.cookie('jwt', token, cookieOptions);
+                    
+                    res.redirect("/home");
+                }
+                else {
+                    responses.messageErr = "La contraseña ingresada es inválida."
+                    res.redirect("/login");
+                } 
+            });
+        } else {
+            responses.messageErr = "El correo no se encuentra registrado en el sistema."
+            console.log(responses.messageErr)
+            res.redirect("/login");
+        }
+    })
 })
 
 app.get("/admin", function(req,res){
