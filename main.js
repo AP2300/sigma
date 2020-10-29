ef38eb4e456ebbb43f71e629fe8db4398ad23ef5
Sebastian Montiel <Smontieltorres@gmail.com>
2020-10-28 21:02:00 -0400

    Problema con Validacion de Datos con Mysql

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var responses = {
+    messageErr:"",
+    messageOK:"",
+}
+
 var express               = require("express"),
     app                   = express(),
     BodyParser            = require("body-parser"),
@@ -46,7 +51,7 @@ app.get("/login", function(req, res){
 })
 
 app.get("/admin", function(req,res){
-    res.render("admin");
+    res.render("admin", {responses});
 })
 
 app.get("/contactanos", function(req, res){
@@ -54,26 +59,56 @@ app.get("/contactanos", function(req, res){
 })
 
 app.post("/register", async (req,res)=>{
+    var redirect = false;
     console.log(req.body.data);
     registerData = req.body.data;
-    DB.query("SELECT Correo FROM Users WHERE Correo = ?", [registerData.email], async (error, results)=>{
+
+    DB.query("SELECT Nombre FROM Users WHERE Nombre = ?", [registerData.name], (error, results)=>{
         if (error){
+            console.log(1);
             console.log(error);
         }
         if(results.length>0){
-            res.render("admin",{messageErr:"el email se encuentra en uso."});
+            console.log(1.1);
+            responses.messageErr = "El Nombre ya esta registrado.";
+            responses.messageOK = "";
+            redirect = true;
         }
     })
+
+    DB.query("SELECT Correo FROM Users WHERE Correo = ?", [registerData.email], (error, results)=>{
+        if (error){
+            console.log(2);
+            console.log(error);
+        }
+        if(results.length>0){
+            console.log(2.1);
+            responses.messageErr = "El email ya esta registrado.";
+            responses.messageOK = "";  
+            redirect = true;
+        }
+    }) 
+
     let hash = await Bcrypt.hash(registerData.pass, 8);
-    DB.query("INSERT INTO Users SET ? ",{
-        Nombre:registerData.name,
-        Correo:registerData.email,
-        Clave:hash
-    }, (err, result)=>{
-        console.log("hola");
-        if(err) console.log(err);
-        else res.render("admin", {messageOK:"usuario registrado correctamente"})
-    })
+
+    
+    if (responses.messageErr !== "") {
+        res.redirect("/admin");
+    } else if (responses.messageErr === "") {
+        DB.query("INSERT INTO Users SET ? ",{
+            Nombre:registerData.name,
+            Correo:registerData.email,
+            Clave:hash
+        }, (err, result)=>{
+            if(err) console.log(err)
+            else {
+                console.log(3);
+                responses.messageOK = "El registro fue hecho satisfactoriamente.";
+                responses.messageErr = ""; 
+                res.redirect("/admin");
+            }
+        })
+    }
 })
 
 ///////////////////////
