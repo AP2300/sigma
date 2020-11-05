2ec7225ea53c9f50af0f2a8a55218b2573852650
Jesus Romero <jesusenrique150620@gmail.com>
2020-11-05 13:29:59 -0400

    fix database disconnect and adding cursor pointer

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ var DBconfig = {
     user     : '7HTdwmHsRH',
     password : 'aFi1Lr7p3K',
     database : '7HTdwmHsRH'
-  };
+};
 
 var DB;
 DB = Sql.createConnection(DBconfig);
@@ -48,7 +48,7 @@ DB.connect((err) => {
     if(err) console.log(err);
     else console.log("DB conectada");
 })
-setTimeout(handleDisconnect, 30000);
+setTimeout(handleDisconnect, 3000);
 
 //configurado midleware para la sesion//
 
@@ -331,35 +331,13 @@ app.listen(app.get("port"), function(){
 });
 
 function handleDisconnect() {
-    DB.query("select * from USERS_SESSIONS", (err) => {
+    DB.query("select 1", (err) => {
         if(err) console.log(err);
         else {
-            console.log("DB reconectada");
-            setTimeout(handleDisconnect, 30000)
+            //console.log("DB reconectada");
+            setTimeout(handleDisconnect, 3000)
         }
     });
-    /*
-    DB = Sql.createConnection(DBconfig); // Recreate the connection, since
-                                                    // the old one cannot be reused.
-  
-    DB.connect(function(err) {              // The server is either down
-      if(err) {                                     // or restarting (takes a while sometimes).
-        console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 500); // We introduce a delay before attempting to reconnect,
-      }else{
-          console.log("DB conectada");
-          return setTimeout(handleDisconnect, 30000);
-      }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meandntime.
-                                            // If you're also serving http, display a 503 error.
-    DB.on('error', function(err) {
-      console.log('db error', err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
-      } else {                                      // connnection idle timeout (the wait_timeout
-        throw err;                                  // server variable configures this)
-      }
-    });*/
 }
 
 function IsAuthenticated(data){
