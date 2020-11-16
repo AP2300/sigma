b7da832690ee2ac531f7a7e1e1e8eaffd75b6e15
Andres Paredes <andresparedes202@gmail.com>
2020-11-16 17:43:11 -0400

    un comit mas como quien no quiere una cosa

--- main.js
+++ main.js
@@ -1360,7 +1360,7 @@ app.post("/Checkout", (req, res)=>{
                                                 <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 40px; padding-left: 40px; padding-top: 20px; padding-bottom: 15px; font-family: Tahoma, sans-serif"><![endif]-->
                                                 <div style="color:#555555;font-family:Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif;line-height:1.2;padding-top:20px;padding-right:40px;padding-bottom:15px;padding-left:40px;">
                                                 <div style="line-height: 1.2; font-size: 12px; color: #555555; font-family: Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; mso-line-height-alt: 14px;">
-                                                <p style="font-size: 46px; line-height: 1.2; text-align: center; word-break: break-word; mso-line-height-alt: 55px; margin: 0;"><span style="font-size: 46px; color: #003188;"><strong></%Usr.nombre.split("")[0]%>tu pago esta siendo procesado</strong></span></p>
+                                                <p style="font-size: 46px; line-height: 1.2; text-align: center; word-break: break-word; mso-line-height-alt: 55px; margin: 0;"><span style="font-size: 46px; color: #003188;"><strong>tu pago esta siendo procesado</strong></span></p>
                                                 </div>
                                                 </div>
                                                 <!--[if mso]></td></tr></table><![endif]-->
